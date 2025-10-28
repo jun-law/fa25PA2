7240d49e4e9fb87760270646572a22bc069eafa2
jun-law <elaw8024@sdsu.edu>
2025-10-28 12:26:21 -0700

    Finished pop()

--- heap.h
+++ heap.h
@@ -2,9 +2,6 @@
 #define HEAP_H
 
 #include <iostream>
-#include <string>
-#include <stack> // for traversal only
-#include <fstream> // file reading
 using namespace std;
 
 struct MinHeap {
@@ -17,9 +14,7 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
-        // how to check for NEXT available element??
-        // size i
-        weightArr[size] = idx; // index is the value
+        weightArr[size] = idx; // index is the key, counts frequency
         // restore
         size++;
         upheap(size-1, weightArr);
@@ -27,7 +22,21 @@ struct MinHeap {
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
-
+        if (size == 0) {
+            cout << "Nothing to pop" << endl;
+            return -1;
+        }
+        else {
+            // size - 1 is the last spot
+            int pop = weightArr[size - 1];
+            cout << "Selected element to pop is: " << pop << endl;
+            weightArr[0] = pop;
+            downheap(size-1, weightArr);
+            size--; // last element gets cut off
+            cout << "Popped " << pop << endl;
+            downheap(size-1, weightArr);
+            return pop;
+        }
     }
 
     // **** check if the parent < child too, min heap condition
@@ -59,13 +68,15 @@ struct MinHeap {
                 int rightChild = weightArr[(pos * 2) + 2];
                 if (leftChild >= rightChild) {
                     swap( weightArr[(pos * 2) + 1], weightArr[(pos * 2) + 2]);
-                }
+                } // do NOT sort by weight, but rather the data[]
+                // compare both at the same indexes
+
 
             }
             else {
                 break;
 
-            }
+            } // size works with upheap
         }
     }
 
@@ -78,9 +89,9 @@ struct MinHeap {
         int rightChild = weightArr[rightChildIndex];
         int parent = weightArr[pos];
 
-        int length = sizeof(weightArr) / 4;
+        // int length = sizeof(weightArr) / 4;
+        if (leftChildIndex < size && rightChildIndex < size) {
 
-        if (leftChildIndex <= length && rightChildIndex <= length) {
             if (leftChild <= rightChild) {
                 return leftChildIndex;
             }
@@ -94,11 +105,13 @@ struct MinHeap {
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
         // pos is the parent index
-        cout << "start: pos is " << pos << endl;
+        // cout << "start: pos is " << pos << endl;
 
         // loop until end of array
-        while (pos <= (sizeof(weightArr) / 4)) {
+        while (pos <= size) {
             int index = findMinChildIndex(pos, weightArr);
+            cout << "index is: " << index << endl;
+            cout << "min child is: " << weightArr[index] << endl;
             if (index != -1) {
                 swap(weightArr[index], weightArr[pos]);
                 pos = index;
