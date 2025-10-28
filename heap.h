35c1bf44f927b6a4334b75416cdd42c4c001dde0
jun-law <elaw8024@sdsu.edu>
2025-10-27 23:55:05 -0700

    Finished push() + added helper function findMinChildIndex() + fixed upheap()/downheap()

--- heap.h
+++ heap.h
@@ -17,74 +17,94 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
-        // call upheap()
+        // how to check for NEXT available element??
+        // size i
+        weightArr[size] = idx; // index is the value
+        // restore
+        size++;
+        upheap(size-1, weightArr);
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+
     }
 
+    // **** check if the parent < child too, min heap condition
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
-        cout << "start: pos is " << pos << endl;
 
-        int& parent = weightArr[(pos - 1) / 2];
-        int& child = weightArr[pos];
+        int parent = weightArr[(pos - 1) / 2];
+        int child = weightArr[pos];
 
         // while the parent exists
         while (pos > 0) { // pos is updated to the latest parent index
-            // **** check if the parent < child too, min heap condition
             // update all values at beginning so parent and child will keep moving up
             int parentIndex = (pos - 1) / 2;
             parent = weightArr[(pos - 1) / 2];
             child = weightArr[pos];
 
+            // before performing
             // if child is greater than parent
-            cout << "comparing " << child << " with " << parent << endl;
+
             if (child < parent) { // fixed condition
                 // they are references, so the value saves
                 swap(weightArr[pos], weightArr[(pos - 1) / 2]);
-
                 // update child index
                 pos = parentIndex;
+                if ((pos * 2) + 2 >= size) {
+                    continue; // disregards everything below it and continues with while loop
+                }
+                int leftChild = weightArr[(pos * 2) + 1];
+                int rightChild = weightArr[(pos * 2) + 2];
+                if (leftChild >= rightChild) {
+                    swap( weightArr[(pos * 2) + 1], weightArr[(pos * 2) + 2]);
+                }
+
             }
             else {
-                cout << "no more parent indexes" << endl;
                 break;
+
             }
         }
+    }
+
+    int findMinChildIndex (int pos, int weightArr[]) {
+
+        int leftChildIndex = (pos * 2) + 1;
+        int rightChildIndex = (pos * 1) + 2;
+
+        int leftChild = weightArr[leftChildIndex];
+        int rightChild = weightArr[rightChildIndex];
+        int parent = weightArr[pos];
+
+        int length = sizeof(weightArr) / 4;
+
+        if (leftChildIndex <= length && rightChildIndex <= length) {
+            if (leftChild <= rightChild) {
+                return leftChildIndex;
+            }
+            else {
+                return rightChildIndex;
+            }
+        }
+        return -1;
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
         // pos is the parent index
+        cout << "start: pos is " << pos << endl;
 
         // loop until end of array
-        while (pos <= sizeof(weightArr)) {
-            // update, cannot have duplicates!!
-            int leftChildIndex = (pos * 2) + 1;
-            int& leftChild = weightArr[(pos * 2) + 1];
-
-            int rightChildIndex = (pos * 2) + 2;
-            int& rightChild = weightArr[(pos * 2) + 2];
-
-            // check bounds of children
-            if (leftChildIndex <= sizeof(weightArr) && rightChildIndex <= sizeof(weightArr)) {
-                // find smaller child
-                if (leftChild <= rightChild) { // if same left will be swapped
-                    swap(weightArr[pos], weightArr[leftChildIndex]);
-                    pos = leftChildIndex; // parent is now left child
-                }
-                else { // right child is smaller
-                    swap(weightArr[pos], weightArr[rightChildIndex]);
-                    pos = rightChildIndex; // parent is now right child
-                }
+        while (pos <= (sizeof(weightArr) / 4)) {
+            int index = findMinChildIndex(pos, weightArr);
+            if (index != -1) {
+                swap(weightArr[index], weightArr[pos]);
+                pos = index;
             }
             else {
                 break;
-
             }
         }
     }
